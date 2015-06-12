94a6990a78790f37e0d5b72bb02dc7a4143d14dd
Michael Lohmann <mial.lohmann@gmail.com>
2015-06-12 16:44:57 +0200

    Fehler behoben, jetzt löscht sich das kompillierte Programm nicht mehr selber...

--- Messung Kiessee 10.6.15/main.cpp
+++ Messung Kiessee 10.6.15/main.cpp
@@ -23,7 +23,9 @@ int main()
 	   plot << "reset" << endl;
 	   //plot << "set yrange [0:4]" << endl;
 	   plot<< "set title '" << dateiname << "'" << endl;
-	   plot << "p '" << dateiname.c_str() << "' u 1:3" << endl << "pause 0.5"; //Wenn das Fenster immer geöffnet bleiben soll, ist die 10 zu ersetzen mit -1
+	   plot << "set xrange [1200:1500]" << endl;
+	   plot << "set yrange [20:35]" << endl;
+	   plot << "p '" << dateiname.c_str() << "' u 3:2" << endl << "pause 0.3"; //Wenn das Fenster immer geöffnet bleiben soll, ist die 10 zu ersetzen mit -1
 	   plot.close();
 	   system("gnuplot plot.gp");
 	}
