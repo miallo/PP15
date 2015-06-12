4a9d29b797082da3b3e6fa52eae50f1170b5bd20
Michael Lohmann <mial.lohmann@gmail.com>
2015-06-12 17:33:34 +0200

    Zähler bei den Bildern eingefügt. Aber jetzt geht es lecker ESSEN!

--- Messung Kiessee 10.6.15/main.cpp
+++ Messung Kiessee 10.6.15/main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main()
 {
+    int i = 0;
     ifstream in ("Datenpunkte.txt");
     while(!in.eof()){
 	   string dateiname, a;
@@ -22,12 +23,20 @@ int main()
 	   ofstream plot ("plot.gp");
 	   plot << "reset" << endl;
 	   //plot << "set yrange [0:4]" << endl;
-	   plot<< "set title '" << dateiname << "'" << endl;
-	   plot << "set xrange [1200:1500]" << endl;
-	   plot << "set yrange [20:35]" << endl;
-	   plot << "p '" << dateiname.c_str() << "' u 3:2" << endl << "pause 0.3"; //Wenn das Fenster immer geöffnet bleiben soll, ist die 10 zu ersetzen mit -1
+	   plot<< "set title 'noch Schritte: " << 36-i << "     " << dateiname << "'" << endl;
+// Temperatur Ranges:
+	//	   plot << "set xrange [1200:1500]" << endl;
+	//	   plot << "set yrange [20:40]" << endl;
+// Tiefe Range:
+	   plot << "set yrange [1200:1500]" << endl;
+	   plot << "p '" << dateiname.c_str() << "' u 0:3" << endl;
+	  
+	   plot << "pause 0.5"; //Wenn das Fenster immer geöffnet bleiben soll, ist die 10 zu ersetzen mit -1
 	   plot.close();
 	   system("gnuplot plot.gp");
+	   //cin.get(); // Wartet auf Enter
+	   //system("sleep 0.5");
+	   i++;
 	}
     in.close();
     system("rm -f plot.gp");
