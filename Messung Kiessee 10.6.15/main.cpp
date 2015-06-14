afafc25e777feb0e3644158732cb0c56691cca80
Michael Lohmann <mial.lohmann@gmail.com>
2015-06-14 15:57:15 +0200

    Geodimeter jetzt ohne Formatierung

--- Messung Kiessee 10.6.15/main.cpp
+++ Messung Kiessee 10.6.15/main.cpp
@@ -25,11 +25,11 @@ int main()
 	   //plot << "set yrange [0:4]" << endl;
 	   plot<< "set title 'noch Schritte: " << 36-i << "     " << dateiname << "'" << endl;
 // Temperatur Ranges:
-	//	   plot << "set xrange [1200:1500]" << endl;
-	//	   plot << "set yrange [20:40]" << endl;
+		   plot << "set xrange [1200:1500]" << endl;
+		   plot << "set yrange [20:40]" << endl;
 // Tiefe Range:
-	   plot << "set yrange [1200:1500]" << endl;
-	   plot << "p '" << dateiname.c_str() << "' u 0:3" << endl;
+	   //plot << "set yrange [1200:1500]" << endl;
+	   plot << "p '" << dateiname.c_str() << "' u 3:2" << endl;
 	  
 	   plot << "pause 0.5"; //Wenn das Fenster immer geöffnet bleiben soll, ist die 10 zu ersetzen mit -1
 	   plot.close();
