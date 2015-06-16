f4510facf2a97ac1161586e2c189002686a35222
Michael Lohmann <mial.lohmann@gmail.com>
2015-06-16 14:18:08 +0200

    GPS-Daten von Fahrt zum Max-Planck-Campus

---
+++ Messung Kiessee 10.6.15/PlotGPS.plot
@@ -0,0 +1,12 @@
+reset
+la(x)=x*(40000/360)
+lo(x,y)=la(x)*sin(pi/2-pi/180*y)
+
+a = 9.920395
+b = 51.520042
+
+x0 = lo(a,b)
+y0 = la(b)
+c = 15*pi/180
+
+plot 'GPSkomplett_2015_06_16.txt' u (1000*(lo($3,$2)-x0)):(1000*(la($2)-y0)), 'GPSsingle_2015_06_16.txt' u (1000*(lo($3,$2)-x0)):(1000*(la($2)-y0))
--- Messung Kiessee 10.6.15/main.cpp
+++ Messung Kiessee 10.6.15/main.cpp
@@ -22,16 +22,18 @@ int main()
 	   //Automatisches erstellen einer Rahmendatei für Gnuplot und anschließendes plotten
 	   ofstream plot ("plot.gp");
 	   plot << "reset" << endl;
+	   plot << "set terminal postscript color" << endl << "set output '" << dateiname << ".eps'" << endl;
 	   //plot << "set yrange [0:4]" << endl;
 	   plot<< "set title 'noch Schritte: " << 36-i << "     " << dateiname << "'" << endl;
 // Temperatur Ranges:
-		   plot << "set xrange [1200:1500]" << endl;
-		   plot << "set yrange [20:40]" << endl;
+		  // plot << "set xrange [1200:1500]" << endl;
+	//	   plot << "set yrange [20:40]" << endl;
 // Tiefe Range:
 	   //plot << "set yrange [1200:1500]" << endl;
-	   plot << "p '" << dateiname.c_str() << "' u 3:2" << endl;
-	  
-	   plot << "pause 0.5"; //Wenn das Fenster immer geöffnet bleiben soll, ist die 10 zu ersetzen mit -1
+//	   plot << "p '" << dateiname.c_str() << "' u 0:(log($5)), '" << dateiname.c_str() << "' u 0:(($4-1013)/20), '" << dateiname.c_str() << "' u 0:(log($6))" << endl;
+	   plot << "p '" << dateiname.c_str() << "' u 0:($2), '"<< dateiname.c_str() << "' u 0:($10*10)" << endl;//, '" << dateiname.c_str() << "' u 0:
+ 	   plot << "set output" << endl;
+	 //  plot << "pause 0.5"; //Wenn das Fenster immer geöffnet bleiben soll, ist die 10 zu ersetzen mit -1
 	   plot.close();
 	   system("gnuplot plot.gp");
 	   //cin.get(); // Wartet auf Enter
